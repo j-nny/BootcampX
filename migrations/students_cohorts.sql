8e3afcba81b91b7d9a2a58152e60f611dafde5ca
Jenny Nguy <j.nguy809@gmail.com>
2019-10-15 16:44:25 +0000

    create teachers and assistance_requests tables, populate data

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,16 +1,16 @@
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
+  name TEXT NOT NULL,
   start_date DATE,
   end_date DATE
 );
 
 CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255),
-  phone VARCHAR(32),
-  github VARCHAR(255),
+  name TEXT NOT NULL,
+  email TEXT,
+  phone TEXT,
+  github TEXT,
   start_date DATE,
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE

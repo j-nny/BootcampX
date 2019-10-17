072158e732a12511c536a5346acbe967cfabc19a
Jenny Nguy <j.nguy809@gmail.com>
2019-10-17 00:22:26 +0000

    change query to prevent sql injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
 SELECT cohorts.name as cohort, teachers.name as teacher
   FROM assistance_requests
   JOIN teachers ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = 'JUL02'
+  WHERE cohorts.name LIKE $1
   GROUP BY cohorts.name, teachers.name
-  ORDER BY teacher;
-`)
+  ORDER BY teacher
+  LIMIT $2;
+` 
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
